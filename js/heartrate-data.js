129fed79439eaba7b2cf9b4b06374d763f8c871a
Zoe Teoh <zteoh@andrew.cmu.edu>
2018-12-06 00:43:49 -0500

    Added data for step, sleep and heart

---
+++ js/heartrate-data.js
@@ -0,0 +1,3 @@
+var index_heart_rate = 75;
+var last_week_heart = [10, 15, 13, 17, 20, 15, 10];
+var this_week_heart = [16, 5, 11, 9, 12, 9, 15];
---
+++ mobile/js/sleep-data.js
@@ -0,0 +1,3 @@
+var today_sleep = 8.54;
+var last_week_sleep = [6.75, 5.01, 10.39, 7.7, 4.30, 6.49, 7.27]
+var this_week_sleep = [5.15, 5.16, 7.06, 7.23, 5.02, 7.38, 8.54]
